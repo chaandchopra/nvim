afe907c73e82e3958a1bc650b02023ed2b6ce846
chaandchopra <chaandchopra@gmail.com>
2025-12-03 13:04:08 +0530

    fixing lsp shortcuts

--- lua/cc/init.lua
+++ lua/cc/init.lua
@@ -2,22 +2,28 @@ require("cc.set")
 require("cc.remap")
 require("cc.lazy_init")
 
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
 -- Reloads a Lua module without restarting Neovim.
--- function R(name)
---     require("plenary.reload").reload_module(name)
--- end
--- 
--- -- When you yank (copy) text, it quickly highlights what you copied.
--- autocmd('TextYankPost', {
---     group = yank_group,
---     pattern = '*',
---     callback = function()
---         vim.highlight.on_yank({
---             higroup = 'IncSearch',
---             timeout = 40,
---         })
---     end,
--- })
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+-- When you yank (copy) text, it quickly highlights what you copied.
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
 
 -- Building features like IDE 
 -- gd = it will go the definition of the function 
@@ -30,23 +36,23 @@ require("cc.lazy_init")
 -- c-h= Shows function parameter hints while typing.
 -- [d = previous warning 
 -- ]d = next warning 
--- autocmd('LspAttach', {
---     group = ThePrimeagenGroup,
---     callback = function(e)
---         local opts = { buffer = e.buf }
---         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
---         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
---         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
---         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
---         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
---         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
---     end
--- })
--- 
+autocmd('LspAttach', {
+    group = ThePrimeagenGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
