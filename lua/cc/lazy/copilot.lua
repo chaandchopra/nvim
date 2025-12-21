039ac18ef309b74eda91eae3c4907e38428bf46e
chaandchopra <chaandchopra@gmail.com>
2025-12-21 07:05:03 +0530

    fixing the copilot with the chat

--- lua/cc/lazy/copilot.lua
+++ lua/cc/lazy/copilot.lua
@@ -1,22 +1,19 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
+    "github/copilot.vim",
     cmd = "Copilot",
     event = "InsertEnter",
-    opts = {
-      suggestion = { enabled = false },  -- Disable inline suggestions (we use cmp)
-      panel = { enabled = false },       -- Disable panel (optional, enable if you want it)
-      filetypes = {
-        markdown = true,
-        help = true,
-        gitcommit = true,
-        ["."] = true,  -- Enable in all other filetypes
-      },
-    },
   },
   {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    opts = {},  -- copilot-cmp has minimal config; just setup() internally
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
   },
 }
+
+
